c3066cd6b660f7af88e9fc0d3daaf9d788d95b4c
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-06-09 01:05:17 -0300

    [ :mailbox_with_mail: ] | create: server - http > express (module::infra)

--- src/@modules/infra/server/http/express/express.server.ts
+++ src/@modules/infra/server/http/express/express.server.ts
@@ -1,3 +1,28 @@
-import express from 'express';
+import { injectable } from 'inversify';
+import { EXPRESS_CORS_TYPE } from '../../../../../@types/server/http/express/cors.type';
+import { EXPRESS_BODY_PARSER_TYPE } from '../../../../../@types/server/http/express/parser/body.type';
+import { IStartHTTPServerDTO } from '../../../../domain/DTO/server/http/start.dto';
+import { IHTTPServer } from '../../../../domain/server/http/http.server';
 
-export const EXPRESS_SERVER = express();
+import { Express } from 'express';
+
+@injectable()
+export class ExpressServer implements IHTTPServer<Express> {
+  constructor(
+    private readonly engine: Express,
+    private readonly parser: EXPRESS_BODY_PARSER_TYPE,
+    private readonly cors: EXPRESS_CORS_TYPE,
+  ) {}
+
+  async start(DTO?: IStartHTTPServerDTO) {
+    this.engine.listen(DTO?.port, () => {});
+  }
+  async setup() {
+    this.engine.use(this.cors);
+    this.engine.use(this.parser);
+  }
+
+  instance(): Express {
+    return this.engine;
+  }
+}
