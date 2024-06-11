7fed0f099693c2a79bd55223cc25d116949dbc8b
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-06-10 23:17:51 -0300

    [ :syringe: ] | create: module - config (module::infra)

---
+++ src/@modules/infra/config/config.module.ts
@@ -0,0 +1,17 @@
+import { Container } from 'inversify';
+import { CONFIG_REGISTRY } from './config.registry';
+import { CONFIG } from './app/app.config';
+
+export const CONFIG_MODULE = new Container({
+  autoBindInjectable: true,
+  defaultScope: 'Singleton',
+});
+
+CONFIG_MODULE.bind(CONFIG_REGISTRY.CONFIG).toConstantValue(CONFIG);
+CONFIG_MODULE.bind(CONFIG_REGISTRY.ENV).toConstantValue(CONFIG.ENV);
+
+CONFIG_MODULE.bind(CONFIG_REGISTRY.DATABASE.URL).toConstantValue(
+  CONFIG.ENV.DATABASE.URL,
+);
+
+CONFIG_MODULE.bind(CONFIG_REGISTRY.PORT).toConstantValue(CONFIG.ENV.PORT);
