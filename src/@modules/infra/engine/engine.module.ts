3592efab8c251315ea84e7ef913c382540becb90
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-06-11 20:44:26 -0300

    [ :syringe: ] | update: module - engine (module::infra)

--- src/@modules/infra/engine/engine.module.ts
+++ src/@modules/infra/engine/engine.module.ts
@@ -1,9 +1,13 @@
 import { Container } from 'inversify';
 import { SERVER_ENGINE_MODULE } from './server/server.module';
 
+import lazy from 'inversify-inject-decorators';
+
 const _MODULE = new Container({
   autoBindInjectable: true,
   defaultScope: 'Singleton',
 });
 
 export const ENGINE_MODULE = Container.merge(_MODULE, SERVER_ENGINE_MODULE);
+
+export const { lazyInject: injectEngine } = lazy(ENGINE_MODULE);
