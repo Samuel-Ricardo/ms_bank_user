d5e7cff995addd5996f2351d6ab8af4aa2120df0
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-06-11 20:21:09 -0300

    [ :syringe: ] | update: registry - infra > engine (app::module)

--- src/@modules/infra/infra.registry.ts
+++ src/@modules/infra/infra.registry.ts
@@ -1,5 +1,7 @@
 import { CONFIG_REGISTRY } from './config/config.registry';
+import { ENGINE_FACTORY } from './engine/engine.registry';
 
 export const INFRA_REGIYSTRY = {
   CONFIG: CONFIG_REGISTRY,
+  ENGINE: ENGINE_FACTORY,
 };
