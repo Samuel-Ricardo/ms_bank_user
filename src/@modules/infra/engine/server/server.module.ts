48762090a2dbb836bc39412728e1c1cd0dbd7efe
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-06-11 20:11:32 -0300

    [ :syringe: ] | create: module - server (module::infra::engine)

---
+++ src/@modules/infra/engine/server/server.module.ts
@@ -0,0 +1,12 @@
+import { Container } from 'inversify';
+import { HTTP_SERVER_ENGINE_MODULE } from './http/http.module';
+
+const _MODULE = new Container({
+  autoBindInjectable: true,
+  defaultScope: 'Singleton',
+});
+
+export const SERVER_MODULE = Container.merge(
+  _MODULE,
+  HTTP_SERVER_ENGINE_MODULE,
+);
