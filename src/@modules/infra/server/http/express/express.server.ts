632ded1b57f7c7bf3e744867f75cced405e085be
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-06-11 20:41:46 -0300

    [ :cloud: ] | update: server - http > express (module::infra)

--- src/@modules/infra/server/http/express/express.server.ts
+++ src/@modules/infra/server/http/express/express.server.ts
@@ -1,16 +1,21 @@
-import { injectable } from 'inversify';
-import { EXPRESS_CORS_TYPE } from '../../../../../@types/server/http/express/cors.type';
-import { EXPRESS_BODY_PARSER_TYPE } from '../../../../../@types/server/http/express/parser/body.type';
+import { inject, injectable } from 'inversify';
+
 import { IStartHTTPServerDTO } from '../../../../domain/DTO/server/http/start.dto';
 import { IHTTPServer } from '../../../../domain/server/http/http.server';
+import { MODULE } from '../../../../app.registry';
 
 import { Express } from 'express';
+import { EXPRESS_BODY_PARSER_TYPE } from '../../../../../@types/infra/engine/server/http/express/parser/body.type';
+import { EXPRESS_CORS_TYPE } from '../../../../../@types/infra/engine/server/http/express/cors.type';
 
 @injectable()
 export class ExpressServer implements IHTTPServer<Express> {
   constructor(
+    @inject(MODULE.INFRA.ENGINE.SERVER.HTTP.EXPRESS.APP)
     private readonly engine: Express,
+    @inject(MODULE.INFRA.ENGINE.SERVER.HTTP.EXPRESS.PARSER.BODY)
     private readonly parser: EXPRESS_BODY_PARSER_TYPE,
+    @inject(MODULE.INFRA.ENGINE.SERVER.HTTP.EXPRESS.CORS)
     private readonly cors: EXPRESS_CORS_TYPE,
   ) {}
 
