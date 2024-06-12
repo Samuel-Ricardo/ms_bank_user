57cd3c2ea90bbcdf32c7c8ae7ece0245273b2dc7
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-06-11 21:55:14 -0300

    [ :gear: ] | update: config - swagger (app::config::docs)

--- swagger.js
+++ swagger.js
@@ -1,24 +1,24 @@
-const swaggerAutogen = require("swagger-autogen")();
-require("dotenv").config();
+const swaggerAutogen = require('swagger-autogen')();
+require('dotenv').config();
 
 const doc = {
   info: {
-    title: "Bank User Microsservice",
-    description: "Bank User Microsservice Documentation",
+    title: 'Bank User Microsservice',
+    description: 'Bank User Microsservice Documentation',
   },
-  host: process.env.HOST || "localhost:3000",
-  schemes: ["http", "https"],
-  consumes: ["application/json"],
-  produces: ["application/json"],
+  host: process.env.HOST || 'localhost:3000',
+  schemes: ['http', 'https'],
+  consumes: ['application/json'],
+  produces: ['application/json'],
 };
 
-const outputFile = "./docs/swagger.json";
+const outputFile = './docs/swagger.json';
 const routes = [
-  "./src/server.ts",
-  "./src/modules/application/router/http/express/employee/employee.router.ts",
-  "./src/modules/application/router/http/express/app.router.ts",
-  "./src/modules/application/router/http/express/docs.router.ts",
-  "./src/modules/application/router/http/express/**/*.router.ts",
+  //  "./src/server.ts",
+  //  "./src/modules/application/router/http/express/employee/employee.router.ts",
+  //  "./src/modules/application/router/http/express/app.router.ts",
+  //  "./src/modules/application/router/http/express/docs.router.ts",
+  //  "./src/modules/application/router/http/express/**/*.router.ts",
 ];
 
 swaggerAutogen(outputFile, routes, doc);
