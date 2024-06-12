ee15e755353f18e2c3b09b1c549df23e7f078776
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-06-11 21:57:13 -0300

    [ :recycle: ] | refact: move - types (module::infra)

--- src/@types/infra/engine/server/http/express/cors.type.ts
+++ src/@types/infra/engine/server/http/express/cors.type.ts
@@ -1,3 +1,3 @@
-import { EXPRESS_CORS } from '../../../../@modules/infra/engine/server/http/express/express.cors';
+import { EXPRESS_CORS } from '../../../../../../@modules/infra/engine/server/http/express/express.cors';
 
 export type EXPRESS_CORS_TYPE = typeof EXPRESS_CORS;
--- src/@types/infra/engine/server/http/express/parser/body.type.ts
+++ src/@types/infra/engine/server/http/express/parser/body.type.ts
@@ -1,3 +1,3 @@
-import { BODY_PARSER } from '../../../../../@modules/infra/engine/server/http/express/parser/body.parser';
+import { EXPRESS_BODY_PARSER } from '../../../../../../../@modules/infra/engine/server/http/express/parser/body.parser';
 
-export type EXPRESS_BODY_PARSER_TYPE = typeof BODY_PARSER;
+export type EXPRESS_BODY_PARSER_TYPE = typeof EXPRESS_BODY_PARSER;
