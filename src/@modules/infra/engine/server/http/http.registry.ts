4fd18e19f6fa3252d73c05b0de6df7c83eaedf48
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-06-11 21:58:53 -0300

    [ :recycle: ] | refact: module - server

--- src/@modules/infra/engine/server/http/http.registry.ts
+++ src/@modules/infra/engine/server/http/http.registry.ts
@@ -1,9 +1,11 @@
 export const HTTP_SERVER_ENGINE_REGISTRY = {
   EXPRESS: {
-    APP: Symbol.for('MODULE::INFRA::SERVER::HTTP::EXPRESS'),
-    CORS: Symbol.for('MODULE::INFRA::SERVER::HTTP::EXPRESS::CORS'),
+    APP: Symbol.for('MODULE::INFRA::ENGINE::SERVER::HTTP::EXPRESS'),
+    CORS: Symbol.for('MODULE::INFRA::ENGINE::SERVER::HTTP::EXPRESS::CORS'),
     PARSER: {
-      BODY: Symbol.for('MODULE::INFRA::SERVER::HTTP::EXPRESS::BODY_PARSER'),
+      BODY: Symbol.for(
+        'MODULE::INFRA::ENGINE::SERVER::HTTP::EXPRESS::BODY_PARSER',
+      ),
     },
   },
 };
