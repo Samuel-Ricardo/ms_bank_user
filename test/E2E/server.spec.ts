e5bfdd47e41c06e15d430eae627714920d6fb250
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-06-18 23:37:32 -0300

    [ :white_check_mark: ] | test: server - health check (test::server)

---
+++ test/E2E/server.spec.ts
@@ -0,0 +1,11 @@
+import supertest from 'supertest';
+import { SERVER } from '../../src/app';
+
+describe('[E2E] | [SERVER]', () => {
+  it('[E2E] | HEALTH CHECK => [HTTP]', async () => {
+    const response = await supertest(SERVER.instance()).get('/');
+
+    expect(response.status).toBe(200);
+    expect(response.body).toStrictEqual({ data: 'Hello World! :D' });
+  });
+});
