0c0e1c45a83413ad315db0828834a6e35af9bdfa
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-06-27 00:40:36 -0300

    [ :adhesive_bandage: ] | fix: module - merge

--- src/@modules/infra/server/server.module.ts
+++ src/@modules/infra/server/server.module.ts
@@ -1,9 +1,9 @@
 import { Container } from 'inversify';
-import { _MODULE } from './http/http.module';
+import { HTTP_SERVER_MODULE } from './http/http.module';
 
 const _MODULE = new Container({
   autoBindInjectable: true,
   defaultScope: 'Singleton',
 });
 
-export const SERVER_MODULE = Container.merge(_MODULE, _MODULE);
+export const SERVER_MODULE = Container.merge(_MODULE, HTTP_SERVER_MODULE);
