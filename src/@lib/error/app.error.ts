d43a613d6043121f57e99f3db960b678e142a2af
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-06-27 01:11:00 -0300

    [ :no_entry: ] | create: lib - error (app::internal::lib)

--- src/@lib/error/app.error.ts
+++ src/@lib/error/app.error.ts
@@ -1,5 +1,5 @@
 import { IError } from '../../@types/lib/error.type';
-import { IErrorDTO } from '@/modules/domain/DTO/error/app.dto';
+import { IErrorDTO } from '../../@modules/domain/DTO/error/app.dto';
 
 export class AppError extends Error implements IError {
   constructor(
@@ -9,23 +9,5 @@ export class AppError extends Error implements IError {
     public error: boolean = true,
   ) {
     super(message);
-  }
-
-  toStruct(): IErrorDTO {
-    return {
-      message: this.message,
-      status: this.status,
-      data: this.data,
-      error: this.error,
-    };
-  }
-
-  fromStruct(struct: IError | IErrorDTO): AppError {
-    return new AppError(
-      struct.message,
-      struct.status,
-      struct.data,
-      struct.error,
-    );
   }
 }
