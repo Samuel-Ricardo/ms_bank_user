659546ba15ad5e6df3da281b5fe8346d608534f3
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-06-27 02:02:19 -0300

    [ :syringe: ] | update: module - http > middleware (module::infra::server)

--- src/@modules/infra/server/http/http.module.ts
+++ src/@modules/infra/server/http/http.module.ts
@@ -4,6 +4,7 @@ import { HTTPExpressServer } from './express/express.server';
 import { ENGINE_MODULE } from '../../engine/engine.module';
 import { CONFIG_MODULE } from '../../config/config.module';
 import { ROUTER_MODULE } from '../../../application/router/router.module';
+import { MIDDLEWARE_MODULE } from '../../../application/middleware/middleware.module';
 
 const _MODULE = new Container({
   autoBindInjectable: true,
@@ -15,6 +16,7 @@ export const HTTP_SERVER_MODULE = Container.merge(
   ENGINE_MODULE,
   CONFIG_MODULE,
   ROUTER_MODULE,
+  MIDDLEWARE_MODULE,
 );
 
 HTTP_SERVER_MODULE.bind(HTTP_SERVER_REGISTRY.EXPRESS).to(HTTPExpressServer);
