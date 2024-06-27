abc5c4937556cd024433d0d0e462f8beb11756bc
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-06-27 01:19:58 -0300

    [ :no_entry: ] | update: lib - error (app::internal::lib)

--- src/@lib/error/app.error.ts
+++ src/@lib/error/app.error.ts
@@ -4,10 +4,31 @@ import { IErrorDTO } from '../../@modules/domain/DTO/error/app.dto';
 export class AppError extends Error implements IError {
   constructor(
     public message: string,
-    public status: number = 400,
+    public status: number = 500,
     public data?: any,
     public error: boolean = true,
+    public url?: string,
   ) {
     super(message);
+    this.url = url ?? `https://http.cat/status/${status}`;
+  }
+
+  toStruct(): IErrorDTO {
+    return {
+      message: this.message,
+      status: this.status,
+      data: this.data,
+      error: this.error,
+    };
+  }
+
+  static fromStruct(error: IError | IErrorDTO): AppError {
+    return new AppError(
+      error.message,
+      error.status,
+      error.data,
+      error.error,
+      error.url,
+    );
   }
 }
