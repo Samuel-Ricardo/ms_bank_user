3ec70a9a289716c4036463a66d69a0ff5aa14e47
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-28 11:57:31 -0300

    [ :syringe: ] | update: module - middleware > log (module::middleware::express)

--- src/@modules/application/middleware/http/express/express.module.ts
+++ src/@modules/application/middleware/http/express/express.module.ts
@@ -1,6 +1,8 @@
 import { Container } from 'inversify';
 import { EXPRESS_HTTP_MIDDLEWARE_REGISTRY } from './express.registry';
 import { ERROR_MIDDLEWARE } from './error/error.middleware';
+import { ERROR_LOGGER_MIDDLEWARE } from './logger/error/logger.middleware';
+import { LOGGER_MIDDLEWARE } from './logger/logger.middleware';
 
 export const EXPRESS_HTTP_MIDDLEWARE_MODULE = new Container({
   autoBindInjectable: true,
@@ -10,3 +12,11 @@ export const EXPRESS_HTTP_MIDDLEWARE_MODULE = new Container({
 EXPRESS_HTTP_MIDDLEWARE_MODULE.bind(
   EXPRESS_HTTP_MIDDLEWARE_REGISTRY.ERROR,
 ).toConstantValue(ERROR_MIDDLEWARE);
+
+EXPRESS_HTTP_MIDDLEWARE_MODULE.bind(
+  EXPRESS_HTTP_MIDDLEWARE_REGISTRY.LOGGER.REQUEST,
+).toConstantValue(LOGGER_MIDDLEWARE);
+
+EXPRESS_HTTP_MIDDLEWARE_MODULE.bind(
+  EXPRESS_HTTP_MIDDLEWARE_REGISTRY.LOGGER.ERROR,
+).toConstantValue(ERROR_LOGGER_MIDDLEWARE);
