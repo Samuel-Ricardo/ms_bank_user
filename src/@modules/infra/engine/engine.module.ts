4c3a56a6f56cc4a55359fe24026fb57d8c1bda42
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-21 22:47:56 -0300

    [ :syringe: ] | update: module - engine > docs (module::infra)

--- src/@modules/infra/engine/engine.module.ts
+++ src/@modules/infra/engine/engine.module.ts
@@ -2,6 +2,8 @@ import { Container } from 'inversify';
 import { SERVER_ENGINE_MODULE } from './server/server.module';
 
 import lazy from 'inversify-inject-decorators';
+import { ENGINE_REGISTRY } from './engine.registry';
+import { SWAGGER_ENGINE } from './documentation/swagger.engine';
 
 const _MODULE = new Container({
   autoBindInjectable: true,
@@ -9,5 +11,9 @@ const _MODULE = new Container({
 });
 
 export const ENGINE_MODULE = Container.merge(_MODULE, SERVER_ENGINE_MODULE);
+
+ENGINE_MODULE.bind(ENGINE_REGISTRY.DOCS.SWAGGER).toConstantValue(
+  SWAGGER_ENGINE,
+);
 
 export const { lazyInject: injectEngine } = lazy(ENGINE_MODULE);
