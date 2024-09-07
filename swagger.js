1a95c67ca53e1902bb83cc584c75466caee5a45c
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-09-07 13:07:48 -0300

    [ :bookmark_tabs: ] | update: config - docs (app::config)

--- swagger.js
+++ swagger.js
@@ -15,9 +15,8 @@ const doc = {
 const outputFile = './docs/swagger.json';
 const routes = [
   //  "./src/server.ts",
-  //  "./src/modules/application/router/http/express/employee/employee.router.ts",
-  //  "./src/modules/application/router/http/express/app.router.ts",
-  //  "./src/modules/application/router/http/express/docs.router.ts",
+  //  './src/@modules/application/router/http/express/app.router.ts',
+  './src/@modules/application/router/http/express/**/*.router.ts',
   //  "./src/modules/application/router/http/express/**/*.router.ts",
 ];
 
