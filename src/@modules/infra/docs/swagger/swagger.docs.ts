a20d058599a37628b6b3107d2207ca793597253d
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-09-07 11:04:02 -0300

    [ :bookmark_tabs: ] | create: docs - swagger (module::infra)

--- src/@modules/infra/docs/swagger/swagger.docs.ts
+++ src/@modules/infra/docs/swagger/swagger.docs.ts
@@ -1,27 +1,23 @@
 import { RequestHandler } from 'express';
 import { IDocumentation } from '../documentation.interface';
-import { ParamsDictionary } from 'express-serve-static-core';
-import { ParsedQs } from 'qs';
+import { SWAGGER_ENGINE_TYPE } from '../../../../@types/infra/engine/docs/swagger.type';
+import { injectEngine } from '../../engine/engine.module';
+import { MODULE } from '../../../app.registry';
+
+import DOCUMENTATION from '../../../../../docs/swagger.json';
 
 export class SwaggerDocumentation
   implements IDocumentation<RequestHandler, RequestHandler>
 {
-  server(): RequestHandler<
-    ParamsDictionary,
-    any,
-    any,
-    ParsedQs,
-    Record<string, any>
-  > {
-    throw new Error('Method not implemented.');
+  @injectEngine(MODULE.INFRA.ENGINE.DOCS.SWAGGER)
+  private readonly _engine!: SWAGGER_ENGINE_TYPE;
+
+  server(): RequestHandler {
+    return this._engine.serve as any;
   }
-  setup(): RequestHandler<
-    ParamsDictionary,
-    any,
-    any,
-    ParsedQs,
-    Record<string, any>
-  > {
-    throw new Error('Method not implemented.');
+
+  setup(): RequestHandler {
+    //    if (!DOCUMENTATION) throw new Error('Swagger documentation not found');
+    return this._engine.setup(DOCUMENTATION);
   }
 }
