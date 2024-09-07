cd71f18230244d11428369ce18cb5ddee69780d1
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-09-07 11:16:14 -0300

    [ :syringe: ] | create: factory - documentation (module::infra)

---
+++ src/@modules/infra/docs/documentation.factory.ts
@@ -0,0 +1,11 @@
+import { RequestHandler } from 'express';
+import { IDocumentation } from './documentation.interface';
+import { DOCUMENTATION_MODULE } from './documentation.module';
+import { DOCUMENTATION_REGISTRY } from './documentation.registry';
+
+export const DOCUMENTATION_FACTORY = {
+  SWAGGER: () =>
+    DOCUMENTATION_MODULE.get<IDocumentation<RequestHandler, RequestHandler>>(
+      DOCUMENTATION_REGISTRY.SWAGGER,
+    ),
+};
