0b651bda88e321ef9ed41a7c972a28772c3c547f
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-10-30 03:05:36 -0300

    [ :label: ] | update: DTO - create > account (module::domain)

--- src/@modules/domain/DTO/client/create.dto.ts
+++ src/@modules/domain/DTO/client/create.dto.ts
@@ -1,4 +1,9 @@
 export interface ICreateAccountDTO {
   account_number: string;
   cpf: string;
+  bacenId: string;
+  full_name: string;
+  email: string;
+  zip_code: string;
+  birth_date: Date;
 }
