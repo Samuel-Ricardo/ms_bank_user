8aa44b370011f96e0ad655e19623569931123ef9
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-10-30 02:15:11 -0300

    [ :card_file_box: ] | create: repository - account (module::application)

---
+++ src/@modules/application/repository/prisma/account/account.repository.ts
@@ -0,0 +1,23 @@
+import { injectable } from 'inversify';
+import { IAccountRepository } from '../../../../domain/repository/account.repository';
+import { ICreateAccountDTO } from '../../../../domain/DTO/client/create.dto';
+import { IDeleteAccountDTO } from '../../../../domain/DTO/client/delete.dto';
+import { IFindCurrentAccountDTO } from '../../../../domain/DTO/client/find/current.dto';
+import { Account } from '../../../../domain/entity/account.entity';
+import { PrismaEngineSupport } from '../../../support/engine/repository/prisma.support';
+
+@injectable()
+export class AccountPrismaRepository
+  extends PrismaEngineSupport
+  implements IAccountRepository
+{
+  createAccount(DTO: ICreateAccountDTO): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  deleteAccount(DTO: IDeleteAccountDTO): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  findCurrentAccount(DTO: IFindCurrentAccountDTO): Promise<Account> {
+    throw new Error('Method not implemented.');
+  }
+}
