7cf7eee51788930216b82a63e077b9fe651af9d1
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-10-30 01:36:17 -0300

    [ :sparkles: ] | create: use-case - find [current] > account (module::domain)

---
+++ src/@modules/domain/use_case/account/find/current.use_case.ts
@@ -0,0 +1,6 @@
+import { IFindCurrentAccountDTO } from '../../../DTO/client/find/current.dto';
+import { Account } from '../../../entity/account.entity';
+
+export interface IFindCurrentAccountUseCase {
+  execute(DTO: IFindCurrentAccountDTO): Promise<Account>;
+}
