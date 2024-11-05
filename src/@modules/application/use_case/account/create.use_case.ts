9ab8008284709ad443551a4cafb5a7cb30c2ff9c
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-11-05 10:54:58 -0300

    [ :sparkles: ] | implements: use-case - create > account (module::application)

---
+++ src/@modules/application/use_case/account/create.use_case.ts
@@ -0,0 +1,14 @@
+import { injectable } from 'inversify';
+import { ICreateAccountUseCase } from '../../../domain/use_case/account/create.use-case';
+import { ICreateAccountDTO } from '../../../domain/DTO/client/create.dto';
+import { AccountPrismaRepositorySupport } from '../../support/repository/prisma/account.support';
+
+@injectable()
+export class CreateAccountInDatabaseUseCase
+  extends AccountPrismaRepositorySupport
+  implements ICreateAccountUseCase
+{
+  async execute(DTO: ICreateAccountDTO) {
+    this.repository.createAccount(DTO);
+  }
+}
