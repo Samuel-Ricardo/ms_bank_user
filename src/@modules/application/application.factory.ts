a93aa8f70f022a9c8e025b809c9ce6b8e3e0463d
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-11-05 10:38:44 -0300

    [ :factory: ] | update: factory - application [repository] (app::module)

--- src/@modules/application/application.factory.ts
+++ src/@modules/application/application.factory.ts
@@ -1,7 +1,9 @@
 import { MIDDLEWARE_FACTORY } from './middleware/middleware.factory';
+import { REPOSITORY_FACTORY } from './repository/repository.factory';
 import { ROUTER_FACTORY } from './router/router.factory';
 
 export const APPLICATION_FACTORY = {
   ROUTER: ROUTER_FACTORY,
   MIDDLEWARE: MIDDLEWARE_FACTORY,
+  REPOSITORY: REPOSITORY_FACTORY,
 };
