20203fd5439777340916d234b1bb72f6337f1969
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-11-05 13:15:44 -0300

    [ :syringe: ] | update: registry - application [use-case] (app::module)

--- src/@modules/application/application.factory.ts
+++ src/@modules/application/application.factory.ts
@@ -1,9 +1,11 @@
 import { MIDDLEWARE_FACTORY } from './middleware/middleware.factory';
 import { REPOSITORY_FACTORY } from './repository/repository.factory';
 import { ROUTER_FACTORY } from './router/router.factory';
+import { USE_CASE_FACTORY } from './use_case/use_case.factory';
 
 export const APPLICATION_FACTORY = {
   ROUTER: ROUTER_FACTORY,
   MIDDLEWARE: MIDDLEWARE_FACTORY,
   REPOSITORY: REPOSITORY_FACTORY,
+  USE_CASE: USE_CASE_FACTORY,
 };
