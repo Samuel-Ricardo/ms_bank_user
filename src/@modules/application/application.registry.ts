acda78950bbdf318928be969474b9471f6a68c47
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-11-05 11:29:32 -0300

    [ :syringe: ] | update: registry - application [use-case] (app::module)

--- src/@modules/application/application.registry.ts
+++ src/@modules/application/application.registry.ts
@@ -1,9 +1,11 @@
 import { MIDDLEWARE_REGISTRY } from './middleware/middleware.registry';
 import { REPOSITORY_REGISTRY } from './repository/repository.registry';
 import { ROUTER_REGISTRY } from './router/router.registry';
+import { USE_CASE_REGISTRY } from './use_case/use_case.registry';
 
 export const APPLICATION_REGISTRY = {
   ROUTER: ROUTER_REGISTRY,
   MIDDLEWARE: MIDDLEWARE_REGISTRY,
   REPOSITORY: REPOSITORY_REGISTRY,
+  USE_CASE: USE_CASE_REGISTRY,
 };
