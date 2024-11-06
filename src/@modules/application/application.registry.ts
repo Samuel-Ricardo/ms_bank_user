f116d299001c2a71dc2d21fa2c52126e127ce1ec
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-11-06 10:44:50 -0300

    [ :syringe: ] | update: registry - application [service] (app::module)

--- src/@modules/application/application.registry.ts
+++ src/@modules/application/application.registry.ts
@@ -1,6 +1,7 @@
 import { MIDDLEWARE_REGISTRY } from './middleware/middleware.registry';
 import { REPOSITORY_REGISTRY } from './repository/repository.registry';
 import { ROUTER_REGISTRY } from './router/router.registry';
+import { SERVICE_REGISTRY } from './service/service.registry';
 import { USE_CASE_REGISTRY } from './use_case/use_case.registry';
 
 export const APPLICATION_REGISTRY = {
@@ -8,4 +9,5 @@ export const APPLICATION_REGISTRY = {
   MIDDLEWARE: MIDDLEWARE_REGISTRY,
   REPOSITORY: REPOSITORY_REGISTRY,
   USE_CASE: USE_CASE_REGISTRY,
+  SERVICE: SERVICE_REGISTRY,
 };
